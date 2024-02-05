7d98c379b84c7d0599791c2f93f435b7dd6e66b3
row.libunao <row.libunao@gmail.com>
2024-02-05 21:06:33 +0800

    Add user api endpoints /details, /set-as-admin, and /update-password

--- controllers/user.js
+++ controllers/user.js
@@ -4,7 +4,7 @@ const auth = require("../auth");
 // Models
 const User = require("../models/User");
 
-module.exports.registerUser = async (req, res) => {
+module.exports.createUser = async (req, res) => {
   try {
     if (!req.body.email.includes("@")) {
       return res.status(400).send({ error: "Email is not valid" });
@@ -28,7 +28,9 @@ module.exports.registerUser = async (req, res) => {
     }
   } catch (error) {
     console.error("Error in saving:", error);
-    return res.status(500).send({ error: "Error in saving user" });
+    return res
+      .status(500)
+      .send({ error: "Internal Server Error: Occured while saving user" });
   }
 };
 
@@ -57,6 +59,89 @@ module.exports.loginUser = async (req, res) => {
     }
   } catch (error) {
     console.error("Error in finding user: ", error);
-    return res.status(500).send({ error: "Error in finding user" });
+    return res
+      .status(500)
+      .send({ error: "Internal Server Error: Occured while finding user" });
+  }
+};
+
+module.exports.getUserDetails = async (req, res) => {
+  try {
+    const userId = req.user.id;
+
+    const foundUser = await User.findById(userId);
+
+    if (!foundUser) {
+      return res.status(404).send({ error: "User not found" });
+    }
+
+    foundUser.password = undefined;
+    return res.status(200).send({ user: foundUser });
+  } catch (error) {
+    console.error("Error in fetching user details: ", error);
+    return res
+      .status(500)
+      .send({
+        error: "Internal Server Error: Occurred while fetching user details",
+      });
+  }
+};
+
+module.exports.updateUserAsAdmin = async (req, res) => {
+  console.log(req.params);
+  const { userId } = req.params;
+
+  try {
+    const foundUser = await User.findById(userId);
+
+    if (!foundUser) {
+      return res.status(404).send({ error: "User not found" });
+    }
+
+    if (foundUser.isAdmin) {
+      return res.status(400).send({ error: "User is already an Admin" });
+    }
+
+    foundUser.isAdmin = true;
+    await foundUser.save();
+    return res
+      .status(200)
+      .send({ message: "User updated successfully as an admin" });
+  } catch (error) {
+    console.error("Error updating user as admin:", error);
+    return res
+      .status(500)
+      .send({
+        error: "Internal Server Error: Occured while updating user as admin",
+      });
+  }
+};
+
+module.exports.updatePassword = async (req, res) => {
+  try {
+    const { newPassword } = req.body;
+    const { id } = req.user;
+
+    if (newPassword.length < 8) {
+      return res
+        .status(400)
+        .send({ error: "Password must be atleast 8 characters" });
+    }
+
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+    await User.findByIdAndUpdate(id, {
+      password: hashedPassword,
+    });
+
+    return res
+      .status(200)
+      .send({ message: "Password updated successfully" });
+  } catch (error) {
+    console.error("Error updating user password:", error);
+    return res
+      .status(500)
+      .send({
+        error: "Internal Server Error: Failed to update password",
+      });
   }
 };
--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,21 @@
 const express = require("express");
 
+// Middleware
 const userController = require("../controllers/user");
+const { verify, verifyAdmin } = require("../auth");
 
 const router = express.Router();
 
 
 // Routes
-router.post("/", userController.registerUser);
+router.post("/", userController.createUser);
 
 router.post("/login", userController.loginUser);
 
+router.get("/details", verify, userController.getUserDetails);
+
+router.put("/:userId/set-as-admin", verify, verifyAdmin, userController.updateUserAsAdmin);
+
+router.put("/update-password", verify, userController.updatePassword);
 
 module.exports = router;
